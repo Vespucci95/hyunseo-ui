a42b6f8aa702b6375d6ccb60d413b53e84bc8ffc
Vespucci95 <donglonk@naver.com>
2024-03-12 17:42:29 +0900

    🔧 eslint, prettier config

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -1,16 +1,15 @@
-import { ButtonHTMLAttributes, forwardRef, PropsWithChildren } from 'react';
+import React, { ButtonHTMLAttributes, forwardRef, PropsWithChildren } from 'react';
 
-type Props = ButtonHTMLAttributes<HTMLButtonElement>
+type Props = ButtonHTMLAttributes<HTMLButtonElement>;
 
 const Button = forwardRef<HTMLButtonElement, PropsWithChildren<Props>>(({ children, ...rest }, ref) => {
-    return (
-        <button
-            ref={ref}
-            {...rest}
-        >
-            {children}
-        </button>
-    )
-})
+  return (
+    <button ref={ref} {...rest}>
+      {children}
+    </button>
+  );
+});
+
+Button.displayName = 'Button';
 
 export default Button;

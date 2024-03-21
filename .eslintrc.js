edc09234be8ed181ff94fcf1dba58ec3b1c949a6
Vespucci95 <donglonk@naver.com>
2024-03-22 02:51:09 +0900

    🔧 update jest.config babel

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,34 @@
 module.exports = {
-	env: {
-		browser: true,
-		es6: true,
-	},
-	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-		'eslint-config-prettier',
-		'plugin:react/recommended',
-	],
-	overrides: [
-		{
-			env: {
-				node: true,
-			},
-			files: ['.eslintrc.{js,cjs}'],
-			parserOptions: {
-				sourceType: 'script',
-			},
-		},
-	],
-	parser: '@typescript-eslint/parser',
-	parserOptions: {
-		ecmaVersion: 'latest',
-		sourceType: 'module',
-	},
-	plugins: ['@typescript-eslint', 'react'],
-	rules: {
-		indent: ['error', 'tab'],
-		quotes: ['error', 'single'],
-		semi: ['error', 'always'],
-	},
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'eslint-config-prettier',
+    'plugin:react/recommended',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'react'],
+  rules: {
+    indent: 'off',
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+  },
 };

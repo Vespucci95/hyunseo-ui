52d3aab123eeb823b65a5a4b9544486d433ae5ad
Vespucci95 <donglonk@naver.com>
2024-03-25 00:54:22 +0900

    🎨 classnames 적용

--- lib/Button/Button.tsx
+++ lib/Button/Button.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import { ButtonHTMLAttributes, forwardRef, PropsWithChildren } from 'react';
-import './Button.module.scss';
+import styles from './Button.module.scss';
+import cn from 'classnames/bind';
+const cx = cn.bind(styles);
 
 const Button = forwardRef<HTMLButtonElement, PropsWithChildren<ButtonHTMLAttributes<HTMLButtonElement>>>(
-  ({ children, ...rest }, ref) => {
+  ({ children, className, ...rest }, ref) => {
     return (
-      <button ref={ref} {...rest}>
+      <button ref={ref} className={cx('container', className)} {...rest}>
         {children}
       </button>
     );
